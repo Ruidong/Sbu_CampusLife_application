b8d50edd0677a4f9909851d3f810508ae64711e8
Rebornzrd <rebornzrd@gmail.com>
2015-06-02 00:09:58 -0400

    Add basic annotation

--- app/src/main/java/com/example/ruidong/sbu_application/SbuDailyLifePOI.java
+++ app/src/main/java/com/example/ruidong/sbu_application/SbuDailyLifePOI.java
@@ -4,4 +4,23 @@ package com.example.ruidong.sbu_application;
  * Created by Ruidong on 5/30/2015.
  */
 public class SbuDailyLifePOI {
+
+    private String poiLabel;
+    private String poiLocatoin;
+    private String poiTime;
+    private String poiPhone;
+    private Double poiLatitude;
+    private Double poiLongitude;
+    private int poiId;
+
+    public SbuDailyLifePOI(int id, String label,String location, double latitude, double longitude,String time,String phone)
+    {
+        this.poiLabel = label;
+        this.poiLocatoin=location;
+        this.poiTime=time;
+        this.poiPhone=phone;
+        this.poiLatitude = latitude;
+        this.poiLongitude = longitude;
+        this.poiId=id;
+    }
 }
